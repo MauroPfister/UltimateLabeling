87a3940dc8ff9d6a32fa0cf8eb5ff80d16e3ae53
Mauro Pfister <mauro.pfister@epfl.ch>
2020-03-27 23:03:57 +0100

    Make bboxes and anchors thinner

--- ultimatelabeling/models/polygon.py
+++ ultimatelabeling/models/polygon.py
@@ -5,7 +5,7 @@ import math
 class Bbox:
     @staticmethod
     def get_thickness():
-        return 2
+        return 1
 
     @staticmethod
     def get_anchor_size():
@@ -68,7 +68,7 @@ class Bbox:
             self.size[1] *= -1
             self.pos[1] -= self.size[1]
 
-    def get_anchors(self, factor=2):
+    def get_anchors(self, factor=1):
         anchors = {}
 
         xmin, ymin, xmax, ymax = self.x1y1x2y2
